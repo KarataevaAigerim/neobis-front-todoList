215bcc8984268db7e77e17a5f13912d3b8acd281
aigerimkarataeva <ai.gerim@hotmail.com>
2024-05-12 18:43:12 +0600

    changed checboxes to radio buttons and downloaded new font

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-// class TodoApp {
-//     constructor() {
-//         this.form = document.getElementById('todo-form');
-//         this.taskList = document.getElementById('todo-list');
-//         this.taskNameInput = document.getElementById('task-name');
-//         this.init();
-//     }
-
-//     init() {
-//         this.form.addEventListener('submit', (e) => {
-//             e.preventDefault();
-//             this.addTask(this.taskNameInput.value, document.querySelector('input[name="type"]:checked').value);
-//             this.taskNameInput.value = '';
-//         });
-//     }
-
-//     addTask(name, type) {
-//         const taskElement = document.createElement('li');
-//         taskElement.className = `todo-app__task todo-app__task--${type}`;
-//         taskElement.innerHTML = `
-//             <input type="checkbox" class="todo-app__checkbox">
-//             <span class="todo-app__text">${name}</span>
-//             <span class="todo-app__edit">Edit</span>
-//             <span class="todo-app__delete">Delete</span>
-//         `;
-//         this.taskList.appendChild(taskElement);
-
-//         taskElement.querySelector('.todo-app__delete').addEventListener('click', () => taskElement.remove());
-//         taskElement.querySelector('.todo-app__checkbox').addEventListener('change', (e) => {
-//             if (e.target.checked) {
-//                 taskElement.style.textDecoration = 'line-through';
-//             } else {
-//                 taskElement.style.textDecoration = 'none';
-//             }
-//         });
-//     }
-// }
-
-// new TodoApp();
-
 
 class TodoApp {
     constructor() {
@@ -50,36 +10,35 @@ class TodoApp {
     init() {
         this.form.addEventListener('submit', (e) => {
             e.preventDefault();
-            this.addTask(this.taskNameInput.value, document.querySelector('input[name="type"]:checked').value);
-            this.taskNameInput.value = '';
+
+            // Check if a task type is selected and the task name input is not empty
+            const typeInput = document.querySelector('input[name="type"]:checked');
+            if (typeInput && this.taskNameInput.value.trim() !== '') {
+                this.addTask(this.taskNameInput.value, typeInput.value);
+                typeInput.checked = false; // Uncheck the selected task type
+                this.taskNameInput.value = ''; // Clear the task name input
+            }
         });
     }
 
     addTask(name, type) {
         const taskElement = document.createElement('li');
-        taskElement.className = `todo-app__task todo-app__task--${type}`;
+        taskElement.className = `todo__task todo__task--${type}`;
         taskElement.innerHTML = `
-            <input type="checkbox" class="todo-app__checkbox">
-            <span class="todo-app__text">${name}</span>
-            <input type="text" class="todo-app__text-input" value="${name}" style="display: none;">
-            <span class="todo-app__edit">Edit</span>
-            <span class="todo-app__delete">Delete</span>
+            <input type="radio" class="todo__radio" name="radio-${name}">
+            <span class="todo__text">${name}</span>
+            <input type="text" class="todo__text-input" value="${name}" style="display: none;">
+            <span class="todo__edit">Edit</span>
+            <span class="todo__delete">Delete</span>
         `;
         this.taskList.appendChild(taskElement);
 
-        // Delete task
-        taskElement.querySelector('.todo-app__delete').addEventListener('click', () => taskElement.remove());
+        // Add event listeners for delete, edit, and toggle completion
+        taskElement.querySelector('.todo__delete').addEventListener('click', () => taskElement.remove());
 
-        // Toggle completion
-        const checkbox = taskElement.querySelector('.todo-app__checkbox');
-        const textSpan = taskElement.querySelector('.todo-app__text');
-        checkbox.addEventListener('change', (e) => {
-            textSpan.style.textDecoration = e.target.checked ? 'line-through' : 'none';
-        });
-
-        // Edit task
-        const editBtn = taskElement.querySelector('.todo-app__edit');
-        const textInput = taskElement.querySelector('.todo-app__text-input');
+        const editBtn = taskElement.querySelector('.todo__edit');
+        const textInput = taskElement.querySelector('.todo__text-input');
+        const textSpan = taskElement.querySelector('.todo__text');
 
         editBtn.addEventListener('click', () => {
             textSpan.style.display = 'none';
